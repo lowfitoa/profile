300bab13f07348cc68ab1fd2dbbdc1b74d2699b4
lowfitoa <xnick411@dicolabs.kr>
2022-04-15 17:43:32 +0900

    feat: 🎸 router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toa } from "./toa";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Toa />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Toa />} />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 );
--- src/toa.tsx
+++ src/toa.tsx
@@ -1,20 +1,19 @@
 import { Container, HStack, Stack } from "@chakra-ui/layout";
 
-// import { AudioPlayer } from "./components/audio-player";
+import { AudioPlayer } from "./components/audio-player";
 import { Icon } from "./components/icon";
 import { SpacerV } from "./components/spacerV";
 import { TacoTuesday } from "./components/taco-tuesday";
 import { platforms } from "./controllers/platforms";
-
+import { useLocation } from "react-router-dom";
 export const Toa = () => {
   const MAIN_COLOR = "#1F4796";
+  const { pathname } = useLocation();
 
   return (
     <Container>
       <Stack justifyContent={"center"} alignItems={"center"} height="100vh" spacing={0}>
-        <TacoTuesday color={MAIN_COLOR} />
-
-        {/* <AudioPlayer /> */}
+        {pathname === "/lofi" ? <AudioPlayer /> : <TacoTuesday color={MAIN_COLOR} />}
 
         <SpacerV h="32px" />
 
