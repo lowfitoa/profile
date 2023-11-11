11d94e55f7304fb7ac9c717b9b45fa5cf104f60e
lowfitoa <toa@humanscape.io>
2023-11-11 14:28:19 +0300

    fix: 🐛 add mobile view

--- src/page/durak/index.tsx
+++ src/page/durak/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Container, HStack, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, HStack, Stack, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
 import { SpacerV } from "../../components/spacerV";
 
 export const Durak = () => {
@@ -9,6 +10,66 @@ export const Durak = () => {
   const [dashaCount, setDashaCount] = useState<number>(0);
   const [toaCount, setToaCount] = useState<number>(0);
   const [drawCount, setDrawCount] = useState<number>(0);
+
+  if (isMobile) {
+    return (
+      <Stack
+        justifyContent={"center"}
+        alignItems={"center"}
+        height="100vh"
+        spacing={0}
+      >
+        <Text fontSize={90} color="#2AFC7B">
+          DURAK
+        </Text>
+
+        <SpacerV h="22px" />
+        <HStack spacing={"40px"} pb={"40px"}>
+          <Button
+            onClick={() => {
+              if (cozerkIndex === 3) {
+                setCozerkIndex(0);
+                return;
+              }
+              setCozerkIndex(cozerkIndex + 1);
+            }}
+          />
+          <Text
+            color="white"
+            fontSize={"100px"}
+            minW={"100px"}
+            textAlign="center"
+          >
+            {cozerk[cozerkIndex]}
+          </Text>
+
+          <Button
+            onClick={() => {
+              if (cozerkIndex === 0) {
+                setCozerkIndex(3);
+                return;
+              }
+              setCozerkIndex(cozerkIndex - 1);
+            }}
+          />
+        </HStack>
+
+        <Stack w="full" justifyContent={"space-between"} maxW="500px">
+          <MobileCountCard
+            name="Dasha"
+            count={dashaCount}
+            setCount={setDashaCount}
+          />
+          <MobileCountCard
+            name="Draw"
+            count={drawCount}
+            setCount={setDrawCount}
+          />
+          <MobileCountCard name="Toa" count={toaCount} setCount={setToaCount} />
+        </Stack>
+      </Stack>
+    );
+  }
 
   return (
     <Stack
@@ -31,7 +92,7 @@ export const Durak = () => {
             }
             setCozerkIndex(cozerkIndex + 1);
           }}
-        ></Button>
+        />
         <Box px={"60px"} borderWidth={"3px"} borderRadius={16} py="90px">
           <Text
             color="white"
@@ -91,6 +152,47 @@ const CountCard = ({ name, count, setCount }: CountCardProps) => {
           fontSize={"30px"}
           minW={"30px"}
           textAlign={"center"}
+          fontWeight={700}
+        >
+          {count}
+        </Text>
+        <Button
+          variant={"outline"}
+          color={"white"}
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </Button>
+      </HStack>
+    </Stack>
+  );
+};
+
+const MobileCountCard = ({ name, count, setCount }: CountCardProps) => {
+  return (
+    <Stack alignItems={"center"} flex={1} spacing="10px">
+      <Text color="white" fontSize={"30px"}>
+        {name}
+      </Text>
+      <HStack alignItems={"center"}>
+        <Button
+          variant={"outline"}
+          color={"white"}
+          onClick={() => {
+            if (count === 0) {
+              return;
+            }
+            setCount(count - 1);
+          }}
+        >
+          -
+        </Button>
+        <Text
+          color="white"
+          fontSize={"30px"}
+          minW={"30px"}
+          textAlign={"center"}
+          fontWeight={700}
         >
           {count}
         </Text>
