8374ca3765a96a82cd8c3202a40c72e508651cc3
lowfitoa <toa@humanscape.io>
2023-11-11 14:33:56 +0300

    fix: 🐛 delete mouse dot in mobile

--- src/components/custom-dot.tsx
+++ src/components/custom-dot.tsx
@@ -1,9 +1,13 @@
 import "./custom-dot.css";
 import { useMousePosition } from "../hooks/useMousePosition";
+import { isMobile } from "react-device-detect";
 
 export const CustomDot = () => {
   // 1.
   const { x, y } = useMousePosition();
+  if (isMobile) {
+    return <></>;
+  }
   return (
     <>
       {/* 2. */}
